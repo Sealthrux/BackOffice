d1cbadba18adceabd2f52198acea809a1d44b59c
Mariana <marianabatatiz01@gmail.com>
2023-02-02 22:52:40 +0000

    Revert "Utilizadores dashboard perfil"
    This reverts commit 8958fb564d7bcd9d85f4d64a474f6d172b0d2615.

--- Utilizador_Perfil_Dashboard/pi3-incomum-backend/README.md
+++
@@ -1,2 +0,0 @@
-# BackEnd
-BACKEND INCOMUM
--- Utilizador_Perfil_Dashboard/pi3-incomum-frontend-backoffice/src/view/auth-header.js
+++
@@ -1,8 +0,0 @@
-export default function authHeader() {
-    const trabalhadores = JSON.parse(localStorage.getItem('trabalhadores'));
-    if (trabalhadores && trabalhadores.token) {
-    return { Authorization: 'Bearer ' + trabalhadores.token };
-    } else {
-    return {};
-    }
-    }
